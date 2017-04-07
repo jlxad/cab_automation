9281acd4b1116a026adf2388cf19c59e3e72ce4d
sakthixad <sakthi.gurumaharaj@xad.com>
2017-04-07 15:00:49 -0700

    Inserted records into db

--- framework/helpers/prod_data_validation_helper.py
+++ framework/helpers/prod_data_validation_helper.py
@@ -2,11 +2,11 @@ import datetime
 from framework.helpers.mysql_helper import *
 from texttable import Texttable
 
-t= Texttable()
+t = Texttable()
 t.add_row(['Query', 'Previous_build_count', 'New_build_count', 'Difference_in_Count'])
 
 def verify_data_in_db_helper(count, query_number, desc_query):
-
+    
     # Get the count recorded the previous run and verify it with the new count. If the difference is more than 10% raise an exception
     cnx = mysql_connect("test_cab")
     query = "select * from prod_data where query_id='"+query_number+"' ORDER BY id DESC LIMIT 1"
@@ -27,7 +27,7 @@ def verify_data_in_db_helper(count, query_number, desc_query):
     cnx.close()
 
     if today_count != previous_day_count:
-        if diff > (10/100)*int(previous_day_count):
+        if diff > (20/100)*int(previous_day_count):
             raise Exception(" There is something going wrong with the data;User count from previous day ="+str(previous_day_count)+":: User count from today = "+str(today_count))
 
 
