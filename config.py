0ec6ab344e7607692fb31ee0491fd6e0ceb6cc52
sakthixad <sakthi.gurumaharaj@xad.com>
2017-04-11 11:38:44 -0700

    Forgot to uncomment out the api url

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 2:
      API_URL= "http://ec2-52-206-109-219.compute-1.amazonaws.com:8050"
 
     if sys.argv[2] == "qa":
-     API_URL= "http://ec2-35-163-23-118.us-west-2.compute.amazonaws.com:8050"
+     API_URL = "http://ec2-35-163-23-118.us-west-2.compute.amazonaws.com:8050"
 
 if len(sys.argv) > 3:
     file_path = '/media/ephemeral0/xad/userstore/dump.txt'
@@ -16,9 +16,9 @@ else:
     file_path = '/Users/sakthigurumaharaj/Desktop/xad/sakthixad/cab_automation/resources/dump.txt'
 
 
-API_URL = "http://ec2-52-206-109-219.compute-1.amazonaws.com:8050"
+# API_URL = "http://ec2-52-206-109-219.compute-1.amazonaws.com:9050"
 
 brand_id = "1"
-category_id= "581208"
+category_id = "581208"
 
 
