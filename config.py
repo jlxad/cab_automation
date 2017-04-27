e898c432fa7eae608a78f3701701e1102979d0ab
sakthixad <sakthi.gurumaharaj@xad.com>
2017-04-26 17:09:26 -0700

    Added the right url for prod

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_URL_dev = "http://ec2-52-206-132-150.compute-1.amazonaws.com:8050"
 
 if len(sys.argv) > 2:
     if sys.argv[2] == "prod":
-     API_URL= "http://ec2-52-206-109-219.compute-1.amazonaws.com:8050"
+     API_URL= "http://ec2-54-226-203-78.compute-1.amazonaws.com:8050:8050"
 
     if sys.argv[2] == "qa":
      API_URL = "http://ec2-35-163-23-118.us-west-2.compute.amazonaws.com:8050"
@@ -14,11 +14,6 @@ if len(sys.argv) > 3:
 else:
     file_path = '/Users/sakthigurumaharaj/Desktop/xad/sakthixad/cab_automation/resources/dump.txt'
 
-#API_URL = "http://ec2-35-163-23-118.us-west-2.compute.amazonaws.com:8050"
-
-#API_URL = "http://ec2-52-206-109-219.compute-1.amazonaws.com:8050"
-
-# http://ec2-54-226-203-78.compute-1.amazonaws.com:8050
 
 brand_id = "1"
 category_id = "581208"
